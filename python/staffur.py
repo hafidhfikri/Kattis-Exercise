7109d23520b8434636ef9af0106ae88ce2fdeac7
Hafidh Fikri <hafidhfikri.rasyid@gmail.com>
2025-06-12 11:58:38 +0700

    12 Juni 2025

---
+++ python/staffur.py
@@ -0,0 +1,12 @@
+s = input()
+i = s
+vowels = "AIUEO"
+not_v = "QWRTPSDFGHJKLZXCVBNM"
+
+if i in vowels:
+    print("Jebb")
+else:
+    if i in not_v:
+        print("Neibb")
+    else:
+        print("Kannski")
